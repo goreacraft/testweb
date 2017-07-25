39131cd073c1e4858c7f191a8d320787e829e356
goreacraft <goreacraft@gmail.com>
2017-07-25 16:43:08 +0300

    Initial commit by ngx-firebase-bootstrap generator

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,11 @@
+
+
+export const firebaseConfig = {
+    // Paste all this from the Firebase console...
+    apiKey: "AIzaSyDUIHpAJGtGrps5fQJiDI50RzK96TvSJTY",
+    authDomain: "kiki2-f79f9.firebaseapp.com",
+    databaseURL: "https://kiki2-f79f9.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "369224919379"
+};
+
